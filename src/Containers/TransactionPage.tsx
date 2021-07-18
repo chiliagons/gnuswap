53b5c826d1b28c0d9c4638ef15e2f7ccf444f18c
wizgot <wizgotD@protonmail.com>
2021-07-18 20:34:28 +0800

    added number in transaction list

--- src/Containers/TransactionPage.tsx
+++ src/Containers/TransactionPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
-import { Table, TableHeader, TableRow, Divider, Icon, IconText, TableAlignment, EthHashInfo } from '@gnosis.pm/safe-react-components';
+import { Table, TableHeader, TableRow, Text, Divider, Icon, IconText, TableAlignment, EthHashInfo } from '@gnosis.pm/safe-react-components';
 // import Transaction from '../components/Transaction';
 import { MockTransactions } from '../Constants/MockTransactions';
 
 const TransactionPage: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [transactions, setTransactions] = React.useState(MockTransactions);
-  const headerCells:TableHeader[] = [{"id":"sender", "label":"Sender Address"}, {"id":"receiverAddress", "label":"Receiver Address"}, {"id":"status", "label":"Status"}, {"id":"retry", alignment: TableAlignment.right , "label":"Retry"}, ];
+  const headerCells:TableHeader[] = [{"id":"sender", "label":"Sender Address"}, {"id":"receiverAddress", "label":"Receiver Address"}, {"id":"Amount", "label":"Amount"}, {"id":"status", "label":"Status"}, {"id":"retry", alignment: TableAlignment.right , "label":"Retry"}, ];
   const rows: TableRow[]= [{"id":"1","cells":[{"content":<EthHashInfo
   hash='0x7BF0C0259DA2db1Cc9A484945722221c5B800139'
   showCopyBtn
@@ -16,7 +16,8 @@ const TransactionPage: React.FC = () => {
   hash='0x7BF0C0259DA2db1Cc9A484945722221c5B800139'
   showCopyBtn
   shortenHash={4}
-/>}, {"content":<IconText  iconSize='sm' textSize='md' iconType='alert' text='failed' /> }, {alignment: TableAlignment.right , "content":<Icon size='md' type='resync' />}] }];
+/>}, {"content": <Text size="sm">100.123456789 ETH</Text>}, 
+{"content":<IconText  iconSize='sm' textSize='md' iconType='alert' text='failed' /> }, {alignment: TableAlignment.right , "content":<Icon size='md' type='resync' />}] }];
 
   return (
     <div>
